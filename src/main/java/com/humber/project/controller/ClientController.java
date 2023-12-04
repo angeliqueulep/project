c04527fb548764e3ff7be2c3970d165408c57cb4
Angelique Ulep <ulepangelique@gmail.com>
2023-12-04 01:49:14 -0500

    Initial commit

---
+++ src/main/java/com/humber/project/controller/ClientController.java
@@ -0,0 +1,12 @@
+package com.humber.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}

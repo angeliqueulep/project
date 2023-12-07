169535c3376ad7845b0263bbcd20754b04fc257e
rhornedo <115593654+rhornedo@users.noreply.github.com>
2023-12-06 22:34:23 -0500

    Implement Delete logic and finished all Display, the  update will redirect to the  update.html template. Just need to uncomment the th:if="${isAdmin}" and add the checks to the divs when the logic from login is implemented.

--- src/main/java/com/humber/project/controller/MenuItemController.java
+++ src/main/java/com/humber/project/controller/MenuItemController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -24,6 +26,11 @@ public class MenuItemController {
         return "home";
     }
 
+    @PostMapping("/delete-item/{id}")
+    public String deleteItem(@PathVariable Integer id) {
+        menuItemService.deleteMenuItem(id);
+        return "redirect:/home";
+    }
 
 }
 
